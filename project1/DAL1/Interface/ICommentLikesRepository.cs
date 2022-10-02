52c036344f7c68c9f7ea3b845025215d23d9866f
haribashi <antonpaskar2003@gmail.com>
2022-10-02 22:31:54 +0300

    add CommentLikesRepository

--- project1/DAL1/Interface/ICommentLikesRepository.cs
+++ project1/DAL1/Interface/ICommentLikesRepository.cs
@@ -4,5 +4,9 @@ namespace DAL1.Interface;
 
 public interface ICommentLikesRepository : IGenericRepository<CommentLikes>
 {
-    
+
+    Task<int> GetCountLikesByCommentIdAsync(int id);
+
+    Task<int> GetCountDislikesByCommentIdAsync(int id);
+
 }
--- project1/DAL1/Repositories/CommentLikesRepository.cs
+++ project1/DAL1/Repositories/CommentLikesRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using DAL1.Interface;
 using DAL1.Model;
+using Dapper;
 
 namespace DAL1.Repositories;
 
@@ -9,4 +10,22 @@ public class CommentLikesRepository : GenericRepository<CommentLikes>, ICommentL
     public CommentLikesRepository(IDbTransaction transaction) : base("commentLikes", transaction)
     {
     }
+
+    public async Task<int> GetCountLikesByCommentIdAsync(int id)
+    {
+        var count = await Connection.QueryAsync<int>(
+            "select Count(*) from commentLikes c inner join likeDislike l on c.likeId = l.id where c.commentId = @Id and l.body = 'like'",
+            param: new { Id = id },
+            transaction: Transaction);
+        return count.FirstOrDefault(0);
+    }
+
+    public async Task<int> GetCountDislikesByCommentIdAsync(int id)
+    {
+        var count = await Connection.QueryAsync<int>(
+            "select Count(*) from commentLikes c inner join likeDislike l on c.likeId = l.id where c.commentId = @Id and l.body = 'dislike'",
+            param: new { Id = id },
+            transaction: Transaction);
+        return count.FirstOrDefault(0);
+    } 
 }
