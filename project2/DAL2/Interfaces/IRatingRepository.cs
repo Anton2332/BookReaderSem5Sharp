a8fd76a3079be279552d4b57fe95d863dba5705c
haribashi <antonpaskar2003@gmail.com>
2022-10-19 22:05:32 +0300

    add interfaces

---
+++ project2/DAL2/Interfaces/IRatingRepository.cs
@@ -0,0 +1,8 @@
+﻿using DAL2.Entitys;
+
+namespace DAL2.Interfaces;
+
+public interface IRatingRepository : IRepository<Rating>
+{
+    
+}
---
+++ project2/DAL2/Interfaces/IUserBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using DAL2.Entitys;
+
+namespace DAL2.Interfaces;
+
+public interface IUserBookRepository
+{
+    IQueryable<UserBook> Items { get; }
+    Task<IEnumerable<UserBook>> GetAllAsync();
+    Task<UserBook> GetByIdAsync(string id, CancellationToken cancel = default);
+    Task AddAsync(UserBook entity, CancellationToken cancel = default);
+    Task UpdateAsync(UserBook entity, CancellationToken cancel = default);
+    Task RemoveAsync(string id, CancellationToken cancel = default);
+}
