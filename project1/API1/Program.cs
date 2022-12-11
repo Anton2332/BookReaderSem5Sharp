5837571daa3fb0cc232d20289dd33b777225f536
haribashi <antonpaskar2003@gmail.com>
2022-12-11 15:43:44 +0200

    add docker compose

--- project1/API1/Program.cs
+++ project1/API1/Program.cs
@@ -1,75 +1,62 @@
 using System.Data;
-using API1.Consumer;
 using BLL1.Configurations;
 using DAL1;
 using DAL1.Interface;
 using DAL1.Repositories;
-using MassTransit;
-using MassTransit.MultiBus;
-using Microsoft.Data.SqlClient;
 using MySqlConnector;
 
-var builder = WebApplication.CreateBuilder(args);
+// static class Program
+// {
+//     public static void Main(string[] args)
+//     {
+        var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+        builder.Services.AddControllers();
 
-builder.Services.AddHealthChecks();
+        builder.Services.AddHealthChecks();
 
-// builder.Services.AddMassTransit(config =>
-// {
-//     config.AddConsumer<BookConsumer>();
-//
-//     config.UsingRabbitMq((ctx, cfg) =>
-//     {
-//         cfg.Host("rabbitmq://localhost");
-//         
-//         cfg.ReceiveEndpoint("book_comment_create", c =>
-//         {
-//             c.ConfigureConsumer<BookConsumer>(ctx);
-//         });
-//     });
-// });
+        builder.Services.AddTransient<ICommentsRepository, CommentsRepository>();
+        builder.Services.AddTransient<ICommentLikesRepository, CommentLikesRepository>();
 
+        builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
 
-builder.Services.AddTransient<ICommentsRepository, CommentsRepository>();
-builder.Services.AddTransient<ICommentLikesRepository, CommentLikesRepository>();
+        builder.Services.AddMapper();
+        builder.Services.AddServices();
 
-builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
-
-builder.Services.AddMapper();
-builder.Services.AddServices();
-
-builder.Services.AddScoped(s =>
-    new MySqlConnection(
-        builder.Configuration.GetConnectionString("DefaultConnection")
-        )
-);
-builder.Services.AddScoped<IDbTransaction>(s =>
-{
-    MySqlConnection connection = s.GetRequiredService<MySqlConnection>();
-    connection.Open();
-    return connection.BeginTransaction();
-});
+        builder.Services.AddScoped(s =>
+            new MySqlConnection(
+                builder.Configuration.GetConnectionString("DefaultConnection")
+            )
+        );
+        builder.Services.AddScoped<IDbTransaction>(s =>
+        {
+            MySqlConnection connection = s.GetRequiredService<MySqlConnection>();
+            connection.Open();
+            return connection.BeginTransaction();
+        });
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+        var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
-app.UseHttpsRedirection();
+        app.UseHttpsRedirection();
 
-app.UseAuthorization();
+        app.UseAuthorization();
 
-app.MapControllers();
+        app.MapControllers();
 
-app.Run();
+        app.Run();
+
+//     }
+// }
